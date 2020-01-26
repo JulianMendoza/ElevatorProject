197cb57a49f84cd1555983f9b5e7328517a10fc2
Giuseppe Papalia <giuseppe.papalia6@gmail.com>
2020-01-26 17:58:00 -0500

    added elevator button enums

--- ElevatorProject/src/constants/ButtonConstants.java
+++
@@ -1,6 +0,0 @@
-package constants;
-
-public class ButtonConstants {
-	public static final String UPBUTTON="UP";
-	public static final String DOWNBUTTON="DOWN";
-}
---
+++ ElevatorProject/src/constants/ElevatorButton.java
@@ -0,0 +1,6 @@
+package constants;
+
+public enum ElevatorButton {
+	
+	UP, DOWN;
+}
