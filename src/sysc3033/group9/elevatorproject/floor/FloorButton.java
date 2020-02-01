7f00cfb2aef071aff8db0d1acb2a983745dece10
kellyharrison <55846122+kellyharrison@users.noreply.github.com>
2020-02-01 18:51:18 -0500

    Update FloorButton.java

--- src/sysc3033/group9/elevatorproject/floor/FloorButton.java
+++ src/sysc3033/group9/elevatorproject/floor/FloorButton.java
@@ -2,16 +2,26 @@ package sysc3033.group9.elevatorproject.floor;
 
 import sysc3033.group9.elevatorproject.constants.Direction;
 import sysc3033.group9.elevatorproject.util.Button;
-
+/**
+ * FloorButton represents the button on the elevator to choose the direction you want to go
+ *
+ */
 public class FloorButton extends Button {
 
 	private Direction targetDirection;
-
+	
+	/**
+	 * Constructor for FloorButton class
+	 * @param targetDirection is the direction (up/ down) 
+	 */
 	public FloorButton(Direction targetDirection) {
 		super();
 		this.targetDirection = targetDirection;
 	}
-
+	/**
+	 * getter for the direction
+	 * @return the direction (UP/DOWN)
+	 */
 	public Direction getTargetDirection() {
 		return targetDirection;
 	}
