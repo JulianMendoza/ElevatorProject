f4d9b439baf575d6394ebac4dd6c3bb55ca05b9d
Julian Mendoza <julianjohnmendoza@gmail.com>
2020-02-01 12:30:15 -0500

    Unit Tests

--- src/sysc3033/group9/elevatorproject/test/TimeParserTest.java
+++ src/sysc3033/group9/elevatorproject/test/TimeParserTest.java
@@ -1,6 +1,7 @@
 package sysc3033.group9.elevatorproject.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.text.ParseException;
 
@@ -10,18 +11,35 @@ import org.junit.Test;
 import sysc3033.group9.elevatorproject.util.Time;
 
 public class TimeParserTest {
-	private Time time;
+	private Time time, time2;
+	private String str;
+
 	@Before
 	public void initialize() {
-	try {
-		time=new Time("14:05:15.0");
-	} catch (ParseException e) {
-		e.printStackTrace();
-	}
-	}
-	@Test
-	public void parseTest() {
-		assertEquals(time.getHours(),14);
+		str = "14:05:15.0";
+		try {
+			time = new Time("14:05:15.0");
+			time2 = new Time(str);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 
+	@Test
+	public void parseTest() {
+		assertEquals(time.getHours(), 14);
+		assertEquals(time.getMinutes(), 5);
+		assertEquals(time.getSeconds(), 15);
+		assertEquals(time.getMilliseconds(), 0);
+	}
+
+	@Test
+	public void stringTest() {
+		assertEquals(time.toString(), time2.toString());
+	}
+
+	@Test
+	public void equalsTest() {
+		assertTrue(time.equals(time2));
+	}
 }
